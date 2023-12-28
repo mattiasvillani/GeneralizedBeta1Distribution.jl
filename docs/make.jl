8690ea2359a3afeb70c7556a4637dd4bdaad825c
mv <mattias.villani@gmail.com>
2023-12-28 23:40:13 +0100

    fixed the make file for the docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,9 @@ using Documenter
 DocMeta.setdocmeta!(GeneralizedBeta1Distribution, :DocTestSetup, :(using GeneralizedBeta1Distribution); recursive=true)
 
 makedocs(;
+    sitename="GeneralizedBeta1Distribution.jl",
     modules=[GeneralizedBeta1Distribution],
     authors="Mattias Villani",
-    repo="https://github.com/mattiasvillani/GeneralizedBeta1Distribution.jl/blob/{commit}{path}#{line}",
-    sitename="GeneralizedBeta1Distribution.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://mattiasvillani.github.io/GeneralizedBeta1Distribution.jl",
@@ -21,5 +20,4 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/mattiasvillani/GeneralizedBeta1Distribution.jl",
-    devbranch="main",
 )
